4a61e0b051d24ad166a1cc3b00f03a9d1259db01
Biruk King <37373118+BiruKing@users.noreply.github.com>
2021-05-28 14:48:45 +0300

    fifth Commit

--- app/src/main/java/com/ethiop/drivinglicencevalidation/activities/HistoryProfile.java
+++ app/src/main/java/com/ethiop/drivinglicencevalidation/activities/HistoryProfile.java
@@ -12,5 +12,6 @@ public class HistoryProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_profile);
+
     }
 }
